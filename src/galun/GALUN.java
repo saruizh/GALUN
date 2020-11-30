54e04ed6d8c21064e918f96813edfa15d0079028
Sergio Ruiz <saruizsolve@gmail.com>
2020-11-29 19:59:12 -0500

    Proyecto en blanco

---
+++ src/galun/GALUN.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galun;
+
+import java.io.File;
+
+/**
+ *
+ * @author sarui
+ */
+public class GALUN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //File input=new File("input"); Proyecto en blanco
+        //System.out.print(input.getAbsolutePath());
+        //System.out.print(input.exists());
+        
+        
+        
+        
+        
+        
+    }
+    
+}
